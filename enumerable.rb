6fa31dd31ea00bf441934f9347d8392dc7b40106
Samiul Anwar <samiul.anwar@gmail.com>
2016-02-23 19:22:31 -0500

    first method is finally working, need to clean it up

--- enumerable.rb
+++ enumerable.rb
@@ -1,11 +1,22 @@
 module Enumerable
-	def my_each
-	#length=array.length-1
-		for i in 0..array.length do
-			yield
+	class::Array
+
+		def my_each
+			len=self.length
+			ret_array=[]
+			num=0
+			len.times do
+				ret_array<<yield(self[num])
+				num=num+1
+			end
+			return ret_array
 		end
+
 	end
 end
-#after writing your methods put them in here 
 
-arr.each {|i|i+2}
+arr=[1,2,3]
+
+puts arr.my_each {|i|i+2}
+
+#this works 
---
+++ notes.rb
@@ -0,0 +1,7 @@
+arr=[1,2,3]
+
+m=arr.method("reverse")
+
+m.call
+
+#use the above to call a method you've created 
